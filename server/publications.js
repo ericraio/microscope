969ba1dec75c87290c9356522687f2bf600db4c5
Eric Raio <ericraio@gmail.com>
2013-09-08 15:41:57 -0700

    use a single post subscript to ensure that we always see the right post

--- server/publications.js
+++ server/publications.js
@@ -7,3 +7,9 @@ Meteor.publish('comments', function(postId) {
 Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId});
 });
+Meteor.publish('newPosts', function(limit) {
+  return Posts.find({}, {sort: {submitted: -1}, limit: limit});
+});
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
